c076107c6f1e516e2875d4f308072ac41d3ae79b
Puneet <puneetsharma8268@gmail.com>
2023-03-20 15:59:57 +0530

    added addition.java

---
+++ addition.java
@@ -0,0 +1,11 @@
+public class addition {
+
+    public static void main(String[] args){
+
+        int a = 4;
+        int b = 4;
+    
+            System.out.println("the sum is " + (a+b));
+        }
+}
+
