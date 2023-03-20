8fde48bb095acfae29d3a4f42f141f7cecef9377
Puneet <puneetsharma8268@gmail.com>
2023-03-20 11:33:19 +0530

    new changes

--- new.java
+++ new.java
@@ -1,11 +1,11 @@
-public class sum {
+public class new {
 
     public static void main(String[] args){
 
     int a = 5;
     int b = 3;
 
-        System.out.println(a+b);
+        System.out.println("the sum is " + (a+b));
     }
 
 }
