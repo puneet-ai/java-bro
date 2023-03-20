58b4a5cb5d4aea0bd91fa12a268a9b4a51fd85fd
Puneet <puneetsharma8268@gmail.com>
2023-03-20 16:29:07 +0530

    added multiplication for new.java

--- new.java
+++ new.java
@@ -1,11 +1,11 @@
-public class new {
+public class New {
 
     public static void main(String[] args){
 
     int a = 5;
     int b = 3;
 
-        System.out.println("the sum is " + (a+b));
+        System.out.println("the sum is " + (a*b));
     }
 
 }
