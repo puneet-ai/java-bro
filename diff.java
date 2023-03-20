e5225d5ebdfd33d2a6bbd0c61490de29035b14c5
Puneet <puneetsharma8268@gmail.com>
2023-03-20 12:52:59 +0530

    added new msg in diff.java

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff {
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 3;
+    
+            System.out.println("the sum is " + (a+b));
+        }
+}
